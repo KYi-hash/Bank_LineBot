fc67ec9ca34ab9426e912b6bb5cad8b9891f3d87
Kai <ky781959@gamail.com>
2026-05-07 11:56:25 +0900

    feat: 銀行活動通知（第二版）

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Version 2
+# Version 3
 ##########################################################
 from selenium import webdriver
 from selenium.webdriver.safari.options import Options
@@ -7,6 +7,7 @@ import pandas as pd
 import requests
 import json
 import os
+from datetime import datetime
 ##########################################################
 
 ### Step 1: Scraped ###
@@ -17,28 +18,38 @@ def input_new_bank_data():
     driver.get("https://www.nextbank.com.tw/announcement/45c1e31fa40000000285cadc56793797")
 
     scraped_data = []
+    driver.implicitly_wait(5)
     titles = driver.find_elements(By.CLASS_NAME, "itemTitle")
-    for t in titles:
-        content = t.text.strip()
+    times = driver.find_elements(By.CLASS_NAME, "date")
+    for tit, tim in zip(titles, times):
+        content = tit.text.strip()
+        publish_time = tim.text.strip()
         if content:
-            scraped_data.append(content)
-
-    df = pd.DataFrame(scraped_data, columns = ["Title"])
-    df.to_csv("/Users/kai/Desktop/My_Project/Bank_line/Data/bank_news.csv", index=False, encoding="utf-8-sig")
+            scraped_data.append({"Title":content, "Time":publish_time})
     
     driver.quit()
+    return scraped_data
 
 ### Step 2: Data ###
 def Clean_Data():
-    new = pd.read_csv("/Users/kai/Desktop/My_Project/Bank_line/Data/bank_news.csv")
-    old  = pd.read_csv("/Users/kai/Desktop/My_Project/Bank_line/Data/bank_olds.csv")
-    update = set(new["Title"]) - set(old["Title"])
-    if update:
-        clean_data = "\n".join(update)
-        message = f"新活動:\n{clean_data}"
+    activity_date = input_new_bank_data()
+    today_date = "2026/05/06"
+
+    message_content = []
+    for item in activity_date:
+        if item['Time'] == today_date:
+            message_content.append(item['Title'])
+    else:
+        same = False
+    
+    message  = None
+    if message_content:
+        message_content = "\n".join(message_content)
+        message = f"新活動:{message_content}"
+        print(message)
     else:
         pass
-    new.to_csv("/Users/kai/Desktop/My_Project/Bank_line/Data/bank_olds.csv", index=False, encoding="utf-8-sig")
+
     return message
 
 
@@ -64,10 +75,7 @@ def send_line_official(message):
     return response.status_code
 
 if __name__ == "__main__":
-    # 執行 Step 1
-    input_new_bank_data()
-    
-    # 執行 Step 2
+    # 執行 Step 1, 2
     msg_to_send = Clean_Data()
     
     # 執行 Step 3：只有當 msg 有內容時才發送
